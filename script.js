94c14c7ee2768e2005b1820b80d94f38dc19654f
suryalakshmanan0066 <suryalakshmanan006@gmail.com>
2023-04-11 15:35:06 +0530

    student profile changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function validate(){
     var pass = document.getElementById("pass").value;
     var alert = document.getElementById("label")
 
-    if(eid == "ilakya@gmail.com" && pass=="111"){
+    if(eid == "suryalakshmanan@gmail.com" && pass=="111"){
 
         document.getElementById('label').innerHTML="Login Success";
         alert.style.color="green";
@@ -17,4 +17,14 @@ function validate(){
         document.getElementById('label').innerHTML="Invalid Credentials";
         alert.style.color="red";
     }
+}
+
+
+function showstud(){
+    document.getElementById('st').style.display="flex"
+    document.getElementById('so').style.display="none"
+}
+function showstaff(){
+    document.getElementById('st').style.display="none"
+    document.getElementById('so').style.display="flex"
 }
